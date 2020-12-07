a7c383d4c9014292c2583440b04c8d74657f4d88
henokali1 <henokali1@gmail.com>
2020-12-07 20:01:06 +0400

    .

--- app.py
+++ app.py
@@ -4,17 +4,17 @@ from PIL import ImageDraw
 import os
 
 fonts_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'fonts')
-x=50
-y=50
+x=500
+y=500
 r=100
 g=100
 b=100
 
 img = Image.open("tadbeer-cv-template-1.png")
 draw = ImageDraw.Draw(img)
-# font = ImageFont.truetype(<font-file>, <font-size>)
-# font = ImageFont.truetype(os.path.join(fonts_path, 'SansSerif.ttf'), 24)
-draw.text((x, y),"Sample Text dfasddf dfa fda dfa fda dfa dfa fa dfa fasdfasdfasdf\nSample Text dfasddf dfa fda dfa fda dfa dfa fa dfa fasdfasdfasdf\nSample Text dfasddf dfa fda dfa fda dfa dfa fa dfa fasdfasdfasdf\nSample Text dfasddf dfa fda dfa fda dfa dfa fa dfa fasdfasdfasdf\nSample Text dfasddf dfa fda dfa fda dfa dfa fa dfa fasdfasdfasdf\nSample Text dfasddf dfa fda dfa fda dfa dfa fa dfa fasdfasdfasdf\nSample Text dfasddf dfa fda dfa fda dfa dfa fa dfa fasdfasdfasdf\nSample Text dfasddf dfa fda dfa fda dfa dfa fa dfa fasdfasdfasdf\nSample Text dfasddf dfa fda dfa fda dfa dfa fa dfa fasdfasdfasdf\n")
+# font = ImageFont.truetype(os.path.join(fonts_path, 'fonts/SansSerif.ttf'), 24)
+font = ImageFont.truetype('fonts/SansSerif.ttf', 24)
+# draw.text((x, y),"Sample Text")
 
-# draw.text((0, 0),"Sample Text",(255,255,255),font=font)
+draw.text((0, 0),"Sample Text",(255,255,255),font=font)
 img.save('sample-out.png')
